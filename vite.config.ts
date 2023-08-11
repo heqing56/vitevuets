ef50ddd3a3470fbee28a919d273990463b6fb918
heqing <1163003537@qq.com>
2023-08-11 10:56:08 +0800

    解决打包文件过大问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import topLevelAwait from 'vite-plugin-top-level-await'
+import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,10 +17,10 @@ export default defineConfig({
     }),
     topLevelAwait({
       // The export name of top-level await promise for each chunk module
-      promiseExportName: '__tla',
+      promiseExportName: "__tla",
       // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: (i: any) => `__tla_${i}`
-    })
+      promiseImportName: (i: any) => `__tla_${i}`,
+    }),
   ],
   resolve: {
     alias: {
@@ -31,6 +31,23 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `@import "./src/assets/scss/index.scss";`, // 此处全局的scss文件
+      },
+    },
+  },
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
     },
   },
